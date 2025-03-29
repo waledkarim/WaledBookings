418c91f4d252394eea82cb0df938db7f70bfef24
Waled Karim <waledkarim445@gmail.com>
2025-03-29 15:23:15 +0600

    render change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
